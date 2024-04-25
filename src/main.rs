f90d5606b4f2264c9dc3205bb635b28315b5d009
clshu <clshu2020@gmail.com>
2024-04-25 14:27:26 +0800

    Another way to return cusome error

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ fn main() -> Result<()> {
 }
 
 fn list_files(path: &str) -> Result<Vec<String>> {
-    let files: Vec<String> = fs::read_dir(path)
-        .map_err(|ex| format!("errpr whil reading dir: cause {ex} "))?
+    let files: Vec<String> = fs::read_dir(path)?
         .filter_map(|re| re.ok())
         .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
         .filter_map(|e| e.file_name().into_string().ok())
         .collect();
+    if files.is_empty() {
+        return Err(("Can not list empty folder").into());
+    }
     Ok(files)
 }
