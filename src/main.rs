88307bcc4777fc593895d3caf33ec1aed1fde498
clshu <clshu2020@gmail.com>
2024-04-25 13:54:59 +0800

    Use '?' to bubble up the error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use std::fs;
+
+pub type Result<T> = core::result::Result<T, Error>;
+pub type Error = Box<dyn std::error::Error>; // for early development
+
+fn main() -> Result<()> {
+    let files = list_files(".");
+
+    println!("{files:#?}");
+
+    Ok(())
+}
+
+fn list_files(path: &str) -> Result<Vec<String>> {
+    let files: Vec<String> = fs::read_dir(path)?
+        .filter_map(|re| re.ok())
+        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+        .filter_map(|e| e.file_name().into_string().ok())
+        .collect();
+    Ok(files)
 }
