01160c1d0400eb7feb15e8a43aa98a6b80ad9c30
clshu <clshu2020@gmail.com>
2024-04-25 14:33:06 +0800

    Refactor code and invoke Result from a different module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-#![allow(unused)]
-use std::fs;
-
 pub type Result<T> = core::result::Result<T, Error>;
 pub type Error = Box<dyn std::error::Error>; // for early development
+
+mod fs;
+
+use crate::fs::list_files;
 
 fn main() -> Result<()> {
     let files = list_files(".");
@@ -10,16 +11,4 @@ fn main() -> Result<()> {
     println!("{files:#?}");
 
     Ok(())
-}
-
-fn list_files(path: &str) -> Result<Vec<String>> {
-    let files: Vec<String> = fs::read_dir(path)?
-        .filter_map(|re| re.ok())
-        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
-        .filter_map(|e| e.file_name().into_string().ok())
-        .collect();
-    if files.is_empty() {
-        return Err(("Can not list empty folder").into());
-    }
-    Ok(files)
 }
